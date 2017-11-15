5e4ada15592326f5dc2048ddab734318dc162a81
Y-Ysss <y-ysss@outlook.com>
2017-11-16 00:48:05 +0900

    MyWebpage Major Update

---
+++ docs/js/index.js
@@ -0,0 +1,24 @@
+$(function() {
+
+    loadMain('home');
+    rippleEffect();
+    // let func = {
+    //     home: function() {},
+    //     about: function() {},
+    //     page1: function() {}
+    // };
+
+    $('a').click(function() {
+        loadMain($(this).attr("id"));
+    });
+});
+
+function loadMain(name) {
+    $('#bodyMain').load('html/' + name + '.html', function() {
+        // $(this).hide().fadeIn(800);
+        $(this).children('.card').animate({
+            'margin-top': 0,
+            'opacity' : 1
+            },500);
+    });
+}
